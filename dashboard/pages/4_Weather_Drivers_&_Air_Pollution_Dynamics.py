8dd33aced032ff152fd30aa3aa2a1d3199163ce5
armanghazi <ghaziaskari@gmail.com>
2026-06-08 14:43:39 +0200

    refactor: Remove unused lag and rolling mean features for air quality pollutants

--- dashboard/pages/4_Weather_Drivers_&_Air_Pollution_Dynamics.py
+++ dashboard/pages/4_Weather_Drivers_&_Air_Pollution_Dynamics.py
@@ -73,15 +73,15 @@ SEASON_COLOR = {
 }
 
 LAG_COLS_PM25  = ["PM25_lag_1","PM25_lag_3","PM25_lag_7",
-                   "PM25_lag_14","PM25_lag_30","PM25_lag_90","PM25_lag_365"]
+                   "PM25_lag_30","PM25_lag_90","PM25_lag_365"]
 LAG_COLS_PM10  = ["PM10_lag_1","PM10_lag_3","PM10_lag_7",
-                   "PM10_lag_14","PM10_lag_30","PM10_lag_90","PM10_lag_365"]
+                   "PM10_lag_30","PM10_lag_90","PM10_lag_365"]
 LAG_COLS_NO2   = ["NO2_lag_1","NO2_lag_3","NO2_lag_7",
-                   "NO2_lag_14","NO2_lag_30","NO2_lag_90","NO2_lag_365"]
+                   "NO2_lag_30","NO2_lag_90","NO2_lag_365"]
 LAG_COLS_SO2   = ["SO2_lag_1","SO2_lag_3","SO2_lag_7",
-                   "SO2_lag_14","SO2_lag_30","SO2_lag_90","SO2_lag_365"]
+                  "SO2_lag_30","SO2_lag_90","SO2_lag_365"]
 
-ROLL_COLS_PM25 = ["PM25_roll_mean_7","PM25_roll_mean_14","PM25_roll_mean_30",
+ROLL_COLS_PM25 = ["PM25_roll_mean_7","PM25_roll_mean_30",
                    "PM25_roll_mean_90","PM25_roll_mean_365"]
 
 # --------------------------------------------------
@@ -600,7 +600,6 @@ with tab4:
         f"{lag_poll}_lag_1":   1,
         f"{lag_poll}_lag_3":   3,
         f"{lag_poll}_lag_7":   7,
-        f"{lag_poll}_lag_14":  14,
         f"{lag_poll}_lag_30":  30,
         f"{lag_poll}_lag_90":  90,
         f"{lag_poll}_lag_365": 365,
@@ -653,7 +652,6 @@ with tab4:
             roll_cols_use = [c for c in ROLL_COLS_PM25 if c in base.columns]
             roll_days_map = {
                 "PM25_roll_mean_7":   7,
-                "PM25_roll_mean_14":  14,
                 "PM25_roll_mean_30":  30,
                 "PM25_roll_mean_90":  90,
                 "PM25_roll_mean_365": 365,
